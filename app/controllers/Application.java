055a4b704262005a6a760b2d41fa17f93f4878ac
Samuel Heaney <sheaney@gmail.com>
2014-03-03 22:08:11 -0600

    Move files bootstrap files into appropriate Play app directories

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,11 +8,11 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Iniciar Sesion"));
+        return ok(index.render());
     }
 
     public static Result staff() {
-        return ok(staff.render(""));
+        return ok(staff.render("Juanito"));
     }
 
 }
