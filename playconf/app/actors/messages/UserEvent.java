1bc296c54429b789d967eab37a2e94ecda31aa70
Samuel Heaney <sheaney@gmail.com>
2014-04-20 16:28:57 -0500

    Delete playconf example

--- playconf/app/actors/messages/UserEvent.java
+++
@@ -1,8 +0,0 @@
-package actors.messages;
-
-import com.fasterxml.jackson.databind.JsonNode;
-
-public interface UserEvent {
-
-    public JsonNode json();
-}
--- playconf/app/models/SessionType.java
+++
@@ -1,5 +0,0 @@
-package models;
-
-public enum SessionType {
- Keynote, OneHourTalk;
-}
