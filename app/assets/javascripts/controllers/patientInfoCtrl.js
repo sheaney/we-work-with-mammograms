8c36304a9e6567c566867c0c036a6c661a0feed6
Samuel Heaney <sheaney@gmail.com>
2014-04-30 20:49:45 -0500

    Staff edits patient client side

--- app/assets/javascripts/controllers/patientInfoCtrl.js
+++ app/assets/javascripts/controllers/patientInfoCtrl.js
@@ -5,11 +5,34 @@ patientInfoApp.controller('PatientInfoCtrl', function($scope, PatientInfo) {
 	// Patient id
   $scope.id = $('#patient').data('patient-id');
 
+  // Numbered options
+  $scope.options = [
+    {value: 0, text: '0'},
+    {value: 1, text: '1'},
+    {value: 2, text: '2'},
+    {value: 3, text: '3'},
+    {value: 4, text: '4'},
+    {value: 5, text: '5'},
+    {value: 6, text: '6'},
+    {value: 7, text: '7'},
+    {value: 8, text: '8'},
+    {value: 9, text: '9'}
+  ];
+
+  // True or false
+  $scope.booleans = [
+    {value: true, text: 'Sí'},
+    {value: false, text: 'No'}
+  ];
+
+  $scope.translate = function(value) {
+    return value ? 'Sí' : 'No';
+  };
+
   PatientInfo.query({id: $scope.id}, function(data) {	
     $scope.patient = data;
     setPatientInfoAvailability($scope.patient);
     // need to handle failure
-    $scope.fullName = data.personalInfo.name + " " + data.personalInfo.firstLastName + " " + data.personalInfo.secondLastName;
   });
 
   // Patient info availability
@@ -19,7 +42,20 @@ patientInfoApp.controller('PatientInfoCtrl', function($scope, PatientInfo) {
     $scope.availableStudies      = patient.studies != undefined;
   };
 
-  // orderBy property
+  // onaftersave handlers
+  $scope.submitPersonalInfo = function() {
+    var personalInfo = $scope.patient.personalInfo;
+    console.log(personalInfo);
+    console.log('Submitting personal info');
+  };
+
+  $scope.submitMedicalInfo = function() {
+    var medicalInfo = $scope.patient.medicalInfo;
+    console.log(medicalInfo);
+    console.log('Submitting medical info');
+  };
+
+  // ng orderBy property
   $scope.mostRecent = 'createdAt';
 
 });
