3bb6f5fc38eb3160514be31fa1392bceb733117b
Fernando Mendoza <fermr91@gmail.com>
2014-05-08 23:13:46 -0500

    Test meow passes for non existing patients, migrated to IntelliJ

--- test/controllers/APITest.scala
+++ test/controllers/APITest.scala
@@ -1,7 +1,9 @@
 package controllers
 
 import integration.{UserLogin, PlayBrowserSpec}
-import play.api.test.{TestBrowser, WsTestClient}
+import play.api.test._
+import play.api.test.Helpers._
+import play.api.mvc._
 import models.Staff
 import scala.concurrent.Await
 import scala.concurrent.duration._
@@ -15,25 +17,10 @@ class APITest extends PlayBrowserSpec with UserLogin with WsTestClient{
 
   describe("Get JSON node with patient information"){
     it("should return notFound error if the patient doesn't exist"){
-      Given("There is a logged in staff member")
-      var staff = login[Staff]
-      When("trying to access with an invalid id")
-      go to (host + controllers.routes.API.getPatientInfo(-1).url)
-      pageSource should include ("NOT_FOUND")
-
-      val testBrowser = new TestBrowser(webDriver,Option(host))
-
-      testBrowser.goTo(controllers.routes.API.getPatientInfo(-1).url)
-
-      /*val client = wsCall(controllers.routes.API.getPatientInfo(-1))(port = 9000)
-      val futureResponse = client.get()
-      Then("The server's response status should be equal to notfound (404)")
-      val response = Await.result(futureResponse,Duration.Inf)
-      response.status shouldBe(404)*/
-
-      staff.delete
-      //logout(staff)
-
+      val fakeRequest = FakeRequest("GET","/api/patient/-1/info")
+      val session = Map("id"->"1","timeOfLogin"-> System.currentTimeMillis().toString(),"type"->"STAFF","user"->"Juanito")
+      val Some(result) = route(fakeRequest.withSession(session.toSeq: _*))
+      status(result) shouldBe(404)
     }
 
     it("should return forbidden if the patient is neither owned or borrowed by the staff member"){
