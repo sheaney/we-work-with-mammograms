4f7cc5cf61099a16d2a4a56ffbf2d917e542b501
Claudia Hernández <cghr1990@hotmail.com>
2014-05-13 16:59:05 -0500

    FileWriter changes

--- app/content/FileWriter.java
+++ app/content/FileWriter.java
@@ -37,27 +37,12 @@ public class FileWriter implements Uploader {
                 outputFile.setLastModified(System.currentTimeMillis());
             }
 
-            ImageIO.write(bi, getFileExtension(file), outputFile);
+            ImageIO.write(bi, "png", outputFile);
 
         } catch (Exception e) {
             throw new FileWriterException(e.getMessage(), e);
         }
 
-    }
-
-    public String getFileExtension(File file) throws UploaderException {
-        Path path = Paths.get(file.getPath());
-        String extension = null;
-
-        try {
-            String contentType = Files.probeContentType(path);
-            String [] splitContentType = contentType.split("/");
-            extension = splitContentType.length > 1 ? splitContentType[1] : splitContentType[0];
-        } catch (IOException ioe) {
-            throw new FileWriterException(ioe.getMessage(), ioe);
-        }
-
-        return extension;
     }
 
 }
