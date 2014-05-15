97f2e633383f8708654288297df8a12941be1820
Claudia Hernández <cghr1990@hotmail.com>
2014-05-14 21:35:46 -0500

    Show MammogramCtrl

--- app/assets/javascripts/controllers/showMammogramCtrl.js
+++ app/assets/javascripts/controllers/showMammogramCtrl.js
@@ -1,23 +1,42 @@
 'use strict';
 
-var showMammogramApp = angular.module('showMammogramApp', ['patientInfoServices', 'CookieCtrl']);
-showMammogramApp.controller('showMammogramCtrl', function($scope, PatientInfo, id) {
-	
-//PatientInfo.query({id: 1}, function(data) {	
+var showMammogramApp = angular.module('showMammogramApp', [
+		'mammogramServices', 'CookieCtrl' ]);
+showMammogramApp.controller('showMammogramCtrl',
+		function($scope, Mammogram, id) {
 
-	// Shows Annotations
-	$scope.showAnnotations = //function(study, mammogram){
-		//data.studies[0].mammograms[0];
-		// Hardcoded annotation
-		"1. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <br> 2. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";	//}
-	//});
-	
-	
-});
-  	
+			$scope.init = function(mammogramId) {
+				$scope.mammogramId = mammogramId;
+				console.log(mammogramId);
 
-$(document).ready(function(){
-    $(".annotation").popover({
-        placement : 'bottom'
-    });
+				Mammogram.query({
+					id : mammogramId
+				}, function(data) {
+
+					var mammogram = data;
+					
+					// Shows Annotations
+					$scope.showAnnotations = function() {
+						var arr = mammogram.annotations;
+						var annotations = "<font size='3'>";
+					
+					// Iterates between annotations array
+							for (var d = 0, len = arr.length; d < len; d += 1) {
+								annotations = annotations + "<b>■</b> " + arr[d].content + "<br>";
+						}
+
+						annotations = annotations + "</font>";
+						return annotations;
+					};
+
+				});
+
+			};
+
+		});
+
+$(document).ready(function() {
+	$(".annotation").popover({
+		placement : 'bottom'
+	});
 });
